495119a5fedc18e49644056ed64f3b17929f3bff
tambing <tambingeras@gmail.com>
2021-08-16 17:42:48 +0700

    Commit Lesson  11

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = Vue.createApp({
             title: 'The Final Empire',
             author: 'Erasmus Tambing',
             age: 23,
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -14,6 +16,16 @@ const app = Vue.createApp({
         },
         showHideBook(){
             this.showBook = !this.showBook
+        },
+        handleEvent(e, data){
+            console.log(e, e.type);
+            if (data) {
+                console.log(data);
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
